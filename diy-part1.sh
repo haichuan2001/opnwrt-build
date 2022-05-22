dabe064d3f8b2c89e13846ee107e9e893478db5e
haichuan2001 <60537083+haichuan2001@users.noreply.github.com>
2022-05-22 15:41:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,8 @@ svn co https://github.com/0saga0/OpenClash/trunk/luci-app-openclash package/open
 #svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/openwrt-packages/luci-theme-infinityfreedom
 #git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
 #git clone https://github.com/fw876/helloworld.git package/openwrt-packages/helloworld
-#git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-packages/luci-app-passwall
-#git clone https://github.com/ophub/luci-app-amlogic.git package/openwrt-packages/luci-app-amlogic
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-packages/luci-app-passwall
+git clone https://github.com/ophub/luci-app-amlogic.git package/openwrt-packages/luci-app-amlogic
 #svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-socat package/openwrt-packages/luci-app-socat
 #git clone https://github.com/zzsj0928/luci-app-pushbot.git package/openwrt-packages/luci-app-pushbot
 #svn co https://github.com/liuran001/openwrt-packages/trunk/luci-app-UUGameAcc package/openwrt-packages/luci-app-UUGameAcc
@@ -23,8 +23,8 @@ svn co https://github.com/0saga0/OpenClash/trunk/luci-app-openclash package/open
 #删除重复软件包
 #rm -rf package/lean/luci-app-dockerman package/lean/luci-app-pushbot
 #更新安装feeds
-#./scripts/feeds update -a
-#./scripts/feeds install -a
+./scripts/feeds update -a
+./scripts/feeds install -a
 #设置环境变量
 useVersionInfo=$(git show -s --date=short --format="编译前的最后一次[➦主源码](https://github.com/coolsnowwolf/lede)更新记录:<br/>更新人: %an<br/>更新时间: %cd<br/>更新内容: %s<br/>哈希值: %H")
 echo "useVersionInfo=$useVersionInfo" >> $GITHUB_ENV
